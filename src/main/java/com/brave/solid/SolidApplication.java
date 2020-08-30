76e00ce1aede2c9d884a8e52e4f491557982be33
caoshaojun <shaojun.cao@dbappsecurity.com.cn>
2020-08-30 21:37:43 +0800

    mybatis-generator:example class

--- src/main/java/com/brave/solid/SolidApplication.java
+++ src/main/java/com/brave/solid/SolidApplication.java
@@ -1,10 +1,11 @@
 package com.brave.solid;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@MapperScan("com.brave.solid.domain.mapper")
 public class SolidApplication {
 
 	public static void main(String[] args) {
